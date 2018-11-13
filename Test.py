46d5e75042252a61a2e783ab7a8610c903e06ca8
KarinLee24 <44620440+KarinLee24@users.noreply.github.com>
2018-11-13 15:26:35 -0500

    Create Test.py
    This test code is a game between yourself and the computer. Assuming rock = 1, paper = 2, and scissors = 3, when you run the code choosing between 1 and 3 either rock or scissors will win

---
+++ Test.py
@@ -0,0 +1,9 @@
+from random import randint
+player = input('rock (r), paper (p) or scissors (s)?') 
+print(player, 'vs')
+
+player = input('rock (r), paper (p) or scissors (s)?')
+print(player, 'vs')
+
+chosen = randint(1,3)
+print(chosen)	
